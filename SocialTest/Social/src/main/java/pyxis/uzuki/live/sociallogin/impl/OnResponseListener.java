318c6f1ccadae093cfc5ddebe6275f6432ed4605
DESKTOP-GMROCB4\NG <isaintaria@gmail.com>
2018-07-23 13:42:22 +0900

    필요없는 파일 정리

--- SocialTest/Social/src/main/java/pyxis/uzuki/live/sociallogin/impl/OnResponseListener.java
+++
@@ -1,7 +0,0 @@
-package pyxis.uzuki.live.sociallogin.impl;
-
-import java.util.Map;
-
-public interface OnResponseListener {
-    void onResult(SocialType social, ResultType result, Map<UserInfoType, String> resultMap);
-}
